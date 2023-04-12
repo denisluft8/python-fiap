70562e22a52dbc345401aa9c729aa8112cce50dc
Dênis Augusto Luft <denisluft8@gmail.com>
2023-04-12 17:36:13 -0300

    decisoes for

--- decisões/for/dec_for.py
+++ decisões/for/dec_for.py
@@ -1,2 +1,4 @@
-for numero in range(1, int(input("Digite um numero para determinar o fim: ")), 1):
-    print("	" + str(numero))
+tabuada=int(input("Digite um número para exibir a tabuada: "))
+print("Tabuada do número ", tabuada)
+for valor in range(1,11,1): #valor inicial, valor até e incremento
+    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
